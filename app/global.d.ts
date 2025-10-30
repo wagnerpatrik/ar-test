95ba6f2be1570d24ce3d06d99cee4e12bc2d5285
Patrik Wagner <wagnerpatrik@hotmail.com>
2025-10-30 12:49:45 +0100

    fixed namespace

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,19 +1,17 @@
-declare module "react" {
-  namespace JSX {
-    interface IntrinsicElements {
-      "model-viewer": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
-        src?: string;
-        "ios-src"?: string;
-        poster?: string;
-        alt?: string;
-        "shadow-intensity"?: string;
-        "camera-controls"?: boolean;
-        "auto-rotate"?: boolean;
-        ar?: boolean;
-      };
-    }
+namespace JSX {
+  interface IntrinsicElements {
+    "model-viewer": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    > & {
+      src?: string;
+      "ios-src"?: string;
+      poster?: string;
+      alt?: string;
+      "shadow-intensity"?: string;
+      "camera-controls"?: boolean;
+      "auto-rotate"?: boolean;
+      ar?: boolean;
+    };
   }
 }
